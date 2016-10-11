e629f80521236dcb95ee62e57f4ea4a4b549a3db
K Cartlidge <code@kcartlidge.com>
2016-10-11 20:13:53 +0100

    Sample beers from the GET Beers route.

--- main.go
+++ main.go
@@ -9,24 +9,29 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
-type dummy struct {
-	Name string
-}
+var (
+	// Cellar ... The collection of beer.
+	Cellar Beers
+)
 
 func GetBeers(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	w.Header().Set("Content-Type", "application/json")
-	result := dummy{"Here are some beers!"}
-	json.NewEncoder(w).Encode(result)
+	json.NewEncoder(w).Encode(Cellar)
 }
 
-func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
+func populateBeers() {
+	Cellar = []Beer{
+		Beer{Id: 1, Name: "Beer 1"},
+		Beer{Id: 2, Name: "Beer 2"},
+	}
 }
 
 func main() {
+	populateBeers()
+
 	router := httprouter.New()
 	router.GET("/beers", GetBeers)
-	router.GET("/hello/:name", Hello)
+	//router.GET("/hello/:name", Hello)
 
 	fmt.Println("The Beer server is on tap now.")
 	log.Fatal(http.ListenAndServe(":8080", router))
