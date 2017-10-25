71ba3fbfc0ac0bdfe9e1d4da4c9904d73085fb5c
Ishan Yapa <ishanyapa@gmail.com>
2017-10-25 05:52:15 +0000

    Added 10 beers

--- data.go
+++ data.go
@@ -9,8 +9,16 @@ import (
 // PopulateBeers populates the Cellar variable with Beers
 func PopulateBeers() {
 	Cellar = []model.Beer{
-		model.Beer{ID: 1, Name: "Beer 1"},
-		model.Beer{ID: 2, Name: "Beer 2"},
+		model.Beer{ID: 1, Name: "Coors Light"},
+		model.Beer{ID: 2, Name: "Brahma"},
+		model.Beer{ID: 3, Name: "Heineken"},
+		model.Beer{ID: 4, Name: "Carlsberg"},
+		model.Beer{ID: 5, Name: "Skol"},
+		model.Beer{ID: 6, Name: "Tsingtao"},
+		model.Beer{ID: 7, Name: "Bud Light"},
+		model.Beer{ID: 8, Name: "Budweiser"},
+		model.Beer{ID: 9, Name: "Corona"},
+		model.Beer{ID: 10, Name: "Yanjing"},
 	}
 }
 
