495b9982c827c0ec979aab371fbf4d2f275d6091
Victor Alves <vhugo.alves@gmail.com>
2017-10-28 06:16:28 +0200

    Add http.Error and other requests from the PR

--- handlers.go
+++ handlers.go
@@ -74,36 +74,31 @@ func AddBeer(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 
 // AddBeerReview adds a new review for a beer
 func AddBeerReview(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	w.Header().Set("Content-Type", "application/json")
-
 	ID, err := strconv.Atoi(ps.ByName("id"))
 	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		json.NewEncoder(w).Encode(fmt.Sprintf("%s is not a valid Beer ID, it must be a number.", ps.ByName("id")))
+		http.Error(w, fmt.Sprintf("%s is not a valid Beer ID, it must be a number.", ps.ByName("id")), http.StatusBadRequest)
 		return
 	}
 
 	for _, v := range Cellar {
 		if v.ID == ID {
-			decoder := json.NewDecoder(r.Body)
-
 			var newReview model.Review
+
+			decoder := json.NewDecoder(r.Body)
 			err := decoder.Decode(&newReview)
 
-			newReview.BeerID = ID
-
 			if err != nil {
-				w.WriteHeader(http.StatusBadRequest)
-				json.NewEncoder(w).Encode(http.StatusBadRequest)
-				fmt.Println("Bad beer - this will be a HTTP status code soon!")
+				http.Error(w, "Failed to parse review", http.StatusBadRequest)
+
 			} else {
+				newReview.BeerID = ID
 				Reviews = append(Reviews, newReview)
+				w.Header().Set("Content-Type", "application/json")
 				json.NewEncoder(w).Encode("New beer review added.")
 			}
 			return
 		}
 	}
 
-	w.WriteHeader(http.StatusNotFound)
-	json.NewEncoder(w).Encode("The beer selected for the review does not exist.")
+	http.Error(w, "The beer selected for the review does not exist.", http.StatusBadRequest)
 }
